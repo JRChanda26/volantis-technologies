50a4c6e298d283a8617e06c3a71b0f19ba746f19
pragatikarout <pragatika.r@iosys.in>
2024-12-05 16:52:48 +0530

    dark bg removed

--- src/app/homepage/StaffSmarter.tsx
+++ src/app/homepage/StaffSmarter.tsx
@@ -74,7 +74,7 @@ function StaffSmarter() {
           left: 0,
           width: "100%",
           height: "100%",
-          backgroundColor: "rgba(0, 0, 0, 0.9)", 
+          backgroundColor: "rgba(0, 0, 0, 0.5)", 
           zIndex: -1, 
         }}
       ></div>
