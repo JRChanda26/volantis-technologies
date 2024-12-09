3ce4317f34e507e13e052b0b90350005de6db7b7
pragatikarout <pragatika.r@iosys.in>
2024-12-09 16:08:01 +0530

    regular font added in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,12 @@ import type { Metadata } from "next";
 import { Poppins } from 'next/font/google';
 import "./globals.css";
 
-const poppinsFont = Poppins({ weight: '700', subsets: ['latin'], variable: '--font-poppins', });
+// const poppinsFont = Poppins({ weight: '700', subsets: ['latin'], variable: '--font-poppins', });
+const poppinsFont = Poppins({
+  subsets: ['latin'],
+  variable: '--font-poppins',
+  weight: ['400', '700'], 
+});
 
 export const metadata: Metadata = {
   title: "Volantis",
